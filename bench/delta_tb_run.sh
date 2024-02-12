13c62c4ec7fea92128a83daeb24874310d4b9a1b
Hao Feng <haofeng1@dt-login03.delta.ncsa.illinois.edu>
2024-02-12 15:12:54 -0600

    update delta slurm script

--- bench/delta_tb_run.sh
+++ bench/delta_tb_run.sh
@@ -6,15 +6,15 @@
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=4
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=32
 #SBATCH --mem=240g
 #SBATCH -t 12:00:00
-#SBATCH --output=delta_kaggle_%j.log
+#SBATCH --output=delta_tb_%j.log
 
 
 module load anaconda3_gpu
 
-cd /jet/home/haofeng1/dlrm_comp
+cd /u/haofeng1/dlrm_comp
 source ~/.bashrc
 # conda activate dlrm
 
@@ -37,27 +37,29 @@ fi
 #echo $dlrm_extra_option
 
 dlrm_pt_bin="python dlrm_s_pytorch.py"
-raw_data="/projects/bcev/haofeng1/Kaggle/raw/train.txt"
-processed_data="/projects/bcev/haofeng1/Kaggle/processed/kaggleAdDisplayChallenge_processed.npz"
+raw_data="/projects/bcev/haofeng1/10M_processed/raw/day"
+processed_data="/projects/bcev/haofeng1/10M_processed/terabyte_processed.npz"
+
 echo "run pytorch ..."
 
-$dlrm_pt_bin --arch-sparse-feature-size=32 --arch-mlp-bot="13-512-256-64-32" --arch-mlp-top="512-256-1" \
+mpirun -np $WORLD_SIZE $dlrm_pt_bin --arch-sparse-feature-size=64 --arch-mlp-bot="13-512-256-64" --arch-mlp-top="512-512-256-1" \
+--max-ind-range=10000000 \
 --data-generation=dataset \
---data-set=kaggle \
+--data-set=terabyte \
 --processed-data-file=$processed_data \
 --raw-data-file=$raw_data \
 --loss-function=bce \
 --round-targets=True \
 --learning-rate=0.1 \
 --nepochs=1 \
---mini-batch-size=128 \
+--mini-batch-size=2048 \
 --print-freq=1024 \
 --print-time \
 --test-freq=1024 \
---test-mini-batch-size=16384 \
---test-num-workers=16 \
+--test-mini-batch-size=10240 \
+--memory-map \
+--data-sub-sample-rate=0.875 \
 --use-gpu \
---enable-compress \
 
 $dlrm_extra_option 2>&1 | tee run_terabyte_pt.log
 
